c5e4cc0d8c6213309f3dfe4c71ce7471d3c79561
karen <mairakarem@hotmail.com>
2021-02-02 10:08:35 -0600

    smileys

--- application/config/smileys.php
+++ application/config/smileys.php
@@ -7,7 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | -------------------------------------------------------------------
 | This file contains an array of smileys for use with the emoticon helper.
 | Individual images can be used to replace multiple smileys.  For example:
-| :-) and :) use the same image replacement.
+| :-) and :) use the same image replacement.sfdsfs
 |
 | Please see user guide for more info:
 | https://codeigniter.com/user_guide/helpers/smiley_helper.html
